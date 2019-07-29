f948d6b0ec9487dc49ee665e8f5d036a25a2342d
student <student@internal.rexnet.nz>
2019-07-30 09:55:12 +1200

    practical test no 7

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 echo "<h1>First Assignment</h1>";
 
+
+// 7
+
+$color = array("red", "blue", "green", "gold", "yellow");
+$names = array("mikaere", "lisa", "shona", "willie", "rachel");
+
+echo "<ol>";
+for($i=0;$i<5;$i++){
+    echo "<li style=\"color:".$color[$i]."\">".$names[$i]."</li>";
+}
+echo "</ol>";
+
 ?>
